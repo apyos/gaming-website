7dbcd039208ae2dec602bae9ea1b2809a148b5c3
Philippe Schommers <philippe@schommers.be>
2022-07-28 17:58:30 +0200

    chore: remove unused css properties in button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,8 +2,7 @@ import { h, ComponentChildren } from "preact";
 import cn from "classnames";
 
 // Config
-const CLASSES =
-  "py-2.5 px-10 border-b hover:ring-0 focus:outline-none focus:ring-0 text-center leading-normal";
+const CLASSES = "py-2.5 px-10 border-b text-center leading-normal";
 const VARIANTS = {
   light: "text-gray-900 bg-white border-gray-100 hover:text-black",
   dark: "text-gray-100 bg-black border-gray-800 hover:text-white",
